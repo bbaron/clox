002691eb2c6df857eaa5a5100b40b356a0bfca63
Bruce Baron <baron.br@gmail.com>
2019-05-20 16:27:29 -0400

    Chunks of Bytecode

---
+++ common.h
@@ -0,0 +1,12 @@
+//
+// Created by Bruce Baron on 2019-05-20.
+//
+
+#ifndef CLOX_COMMON_H
+#define CLOX_COMMON_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif //CLOX_COMMON_H
