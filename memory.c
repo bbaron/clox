604e77f6074ebb26e5a0567608bfbb9bce1d00c4
Bruce Baron <baron.br@gmail.com>
2019-05-21 16:28:41 -0400

    Strings

--- memory.c
+++ memory.c
@@ -2,8 +2,10 @@
 
 #include "common.h"
 #include "memory.h"
+#include "value.h"
+#include "vm.h"
 
-void* reallocate(void* previous, size_t  __unused oldSize, size_t newSize) {
+void *reallocate(void *previous, size_t  __unused oldSize, size_t newSize) {
   if (newSize == 0) {
     free(previous);
     return NULL;
@@ -12,3 +14,22 @@ void* reallocate(void* previous, size_t  __unused oldSize, size_t newSize) {
   return realloc(previous, newSize);
 }
 
+static void freeObject(Obj *object) {
+  switch (object->type) {
+    case OBJ_STRING: {
+      ObjString *string = (ObjString *) object;
+      FREE_ARRAY(char, string->chars, string->length + 1);
+      FREE(ObjString, object);
+      break;
+    }
+  }
+}
+
+void freeObjects() {
+  Obj *object = vm.objects;
+  while (object != NULL) {
+    Obj *next = object->next;
+    freeObject(object);
+    object = next;
+  }
+}
--- memory.h
+++ memory.h
@@ -1,10 +1,16 @@
-//
-// Created by Bruce Baron on 2019-05-20.
-//
-
 #ifndef CLOX_MEMORY_H
 #define CLOX_MEMORY_H
-#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#include "object.h"
+
+#define ALLOCATE(type, count) \
+    (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
+#define FREE(type, pointer) \
+    reallocate(pointer, sizeof(type), 0)
+
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
 
 #define GROW_ARRAY(previous, type, oldCount, count) \
   (type*)reallocate(previous, sizeof(type) * (oldCount), \
@@ -14,6 +20,8 @@
 #define FREE_ARRAY(type, pointer, oldCount) \
     reallocate(pointer, sizeof(type) * (oldCount), 0)
 
-void* reallocate(void* previous, size_t  __unused oldSize, size_t newSize);
+void *reallocate(void *previous, size_t  __unused oldSize, size_t newSize);
+
+void freeObjects();
 
 #endif //CLOX_MEMORY_H
