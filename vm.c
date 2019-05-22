c9897eb83ca205f68346d154956924b82b3fe277
Bruce Baron <baron.br@gmail.com>
2019-05-22 15:09:56 -0400

    Jumping Back and Forth

--- vm.c
+++ vm.c
@@ -74,9 +74,13 @@ static void concatenate() {
   push(OBJ_VAL(result));
 }
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "hicpp-signed-bitwise"
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_SHORT() (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
 #define READ_STRING() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op) \
    do { \
@@ -147,7 +151,7 @@ static InterpretResult run() {
       }
 
       case OP_SET_GLOBAL: {
-        ObjString* name = READ_STRING();
+        ObjString *name = READ_STRING();
         if (tableSet(&vm.globals, name, peek(0))) {
           tableDelete(&vm.globals, name);
           runtimeError("Undefined variable '%s'.", name->chars);
@@ -207,6 +211,22 @@ static InterpretResult run() {
         break;
       }
 
+      case OP_JUMP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip += offset;
+        break;
+      }
+      case OP_LOOP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip -= offset;
+        break;
+      }
+      case OP_JUMP_IF_FALSE: {
+        uint16_t offset = READ_SHORT();
+        if (isFalsey(peek(0))) vm.ip += offset;
+        break;
+      }
+
       case OP_RETURN: {
         return INTERPRET_OK;
       }
@@ -215,9 +235,12 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_SHORT
 #undef BINARY_OP
 #undef READ_STRING
 }
+
+#pragma clang diagnostic pop
 
 InterpretResult interpret(const char *source) {
   Chunk chunk;
